73c627f1bbb3ebf9f8435eb6a66842a38631712c
Tekiter <tekiter8@gmail.com>
2020-12-19 11:39:05 +0900

    Create StaticBadge component

--- components/ShieldsStaticBadge.tsx
+++ components/ShieldsStaticBadge.tsx
@@ -1,17 +1,26 @@
 import { FC } from "react";
+import { BadgeStyle, StaticBadge, staticBadge } from "../utils/badge";
 
-export interface ShieldsBadgeProps {
+export interface ShieldsBadgeProps extends BadgeStyle {
   label: string;
   message?: string;
   color: string;
 }
 
 export const ShieldsStaticBadge: FC<ShieldsBadgeProps> = (props) => {
-  const { label = "", message = "", color = "blue" } = props;
+  const { label = "", message = "", color = "blue", ...styles } = props;
+
+  const badge: StaticBadge = {
+    label,
+    message,
+    color,
+    ...styles,
+  };
 
   return (
     <div>
-      <img src={`https://img.shields.io/badge/${label}-${message}-${color}`} />
+      {/* <img src={staticBadge({ label, message, color })} /> */}
+      <img src={staticBadge(badge)} />
     </div>
   );
 };
---
+++ utils/badge.ts
@@ -0,0 +1,62 @@
+export type BadgeStylesTypes =
+  | "plastic"
+  | "flat"
+  | "flat-square"
+  | "for-the-badge"
+  | "social";
+
+export interface BadgeStyle {
+  style?: BadgeStylesTypes;
+  logo?: string;
+  label?: string;
+  logoColor?: string;
+  logoWidth?: number;
+  link?: string;
+  labelColor?: string;
+  color?: string;
+  cacheSeconds?: number;
+}
+
+export interface StaticBadge extends BadgeStyle {
+  label: string;
+  message?: string;
+  color: string;
+}
+
+function makeQuery(obj: { [key: string]: any }): string {
+  const arr = [];
+
+  for (let key in obj) {
+    arr.push(`${encodeURIComponent(key)}=${encodeURIComponent(obj[key] + "")}`);
+  }
+
+  return arr.join("&");
+}
+
+function escapeParam(value: string): string {
+  let s = encodeURIComponent(value);
+  s = s.replace(/-/g, "--");
+  s = s.replace(/_/g, "__");
+  s = s.replace(/ /g, "_");
+
+  return s;
+}
+
+export function staticBadge(badge: StaticBadge): string {
+  let { label = "", message = "", color = "blue", ...styles } = badge;
+  label = escapeParam(label);
+  message = escapeParam(message);
+  color = escapeParam(color);
+
+  const mainPart = `${label}-${message}-${color}`;
+
+  const query = makeQuery(styles);
+
+  let url = `https://img.shields.io/badge/${mainPart}`;
+
+  if (query !== "") {
+    url += `?${query}`;
+  }
+
+  return url;
+}
