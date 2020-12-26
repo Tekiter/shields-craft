8191460623f5451c933ded25317d6ed627966cc1
Tekiter <tekiter8@gmail.com>
2020-12-26 02:48:59 +0000

    Update gh-pages to output generated at 52e83a4

---
+++ _next/static/VGKhym7Y6u-FaHDZ_e4PG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-23aaa50396aa018a61b2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f238f3f5a72fa559385d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
