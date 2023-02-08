449049671ad1178831ac89e516a5725bf9492f01
Tekiter <tekiter8@gmail.com>
2023-02-09 03:12:14 +0900

    Fix storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    stories: ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
+    stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
